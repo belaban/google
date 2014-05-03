d1888e0841677e99e4a39edce0d57e05c1c68a7b
Bela Ban <belaban@yahoo.com>
2014-05-03 16:26:55 +0200

    ns

--- start-uperf.sh
+++ start-uperf.sh
@@ -4,5 +4,6 @@
 source config.sh
 
 name=$1
+props=http://storage.googleapis.com/jgroups-bucket/tcpping.xml
 
-gcutil ssh ${name} "nohup uperf.sh -props ./google.xml -name ${name} -nohup > /dev/null 2> /dev/null < /dev/null &"
+gcutil ssh ${name} "nohup uperf.sh -props $props -name ${name} -nohup > /dev/null 2> /dev/null < /dev/null &"
