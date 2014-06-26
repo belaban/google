f25276b694a1de38ac1f702d531f98594f107371
Bela Ban <belaban@yahoo.com>
2014-06-26 10:12:01 +0200

    ns

--- config.sh
+++ config.sh
@@ -1,10 +1,8 @@
 
 
-## max number of instances created
-max_cluster_size=500
-
 
 machine_type=n1-highcpu-4
+#machine_type=n1-standard-1
 zone=us-central1-b
 
 OPTS="--image=jgroups2 --disk=jgroups-disk,mode=ro --machine_type=${machine_type} --auto_delete_boot_disk --zone=${zone}"
--- generate-list.sh
+++ generate-list.sh
@@ -2,5 +2,6 @@
 
 #filename=00000000-0000-0000-0000-000000000001.box1.list
 
-gcutil listinstances |grep RUNNING | awk '{print $2, NR, $8":7800 " (NR==1?" T" : " F")}'
+#gcutil listinstances |grep RUNNING | awk '{print $2, NR, $8":7800 " (NR==1?" T" : " F")}'
+gcutil listinstances |grep RUNNING | awk '{print $2, substr($2,match($2,/[0-9]+/),RLENGTH), $8":7800 " (NR==1?" T" : " F")}'
 
