67e8246975f556388f35d1c635371b22bf5011cc
Bela Ban <belaban@yahoo.com>
2014-07-01 12:21:00 +0200

    ns

--- config.sh
+++ config.sh
@@ -7,4 +7,5 @@ machine_type=n1-highcpu-4
 zone=europe-west1-b
 
 OPTS="--image=jgroups2 --disk=jgroups-disk,mode=ro --machine_type=${machine_type} --auto_delete_boot_disk --zone=${zone}"
+OPTS="${OPTS} --max_wait_time=30 --sleep_between_polls=10 --synchronous_mode=false"
 
