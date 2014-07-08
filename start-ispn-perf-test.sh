086778a63565e43819e32638b18a7e306ab812c5
Bela Ban <belaban@yahoo.com>
2014-07-08 09:34:16 +0200

    ns

--- start-ispn-perf-test.sh
+++ start-ispn-perf-test.sh
@@ -3,9 +3,27 @@
 
 source config.sh
 
-name=$1
-uuid=$2
-props=infinispan-google.xml
+if [ $# -ne 3 ];
+    then echo "start-ispn-perf-test.sh <instance ID> <bind-port> <number of nodes>"
+    exit -1
+fi
 
-gcutil ssh ${name} "nohup ispn-perf-test.sh -cfg $props -name ${name} -uuid ${uuid} -nohup > /dev/null 2> /dev/null < /dev/null &"
-#gcutil ssh ${name} "ispn-perf-test.sh -cfg $props -name ${name} -uuid ${uuid} -nohup"
+instance_id=$1
+machine=${prefix}${instance_id}
+bind_port=$2
+num_nodes=$3
+props=infinispan-google.xml
+ssh_args="--log_level=WARNING --ssh_arg=-x --ssh_arg=-oLogLevel=quiet"
+command="gcutil ssh ${ssh_args} ${machine} "
+
+for (( i=1; i<= ${num_nodes}; i++ )) ; {
+    node_name=${machine}.${i}
+    uuid=$((${instance_id} * ${num_nodes} - ${num_nodes} + ${i}))
+    tmp_port=$((${bind_port} + ${i} -1))
+    # echo "machine=${machine}: node: ${node_name} uuid: ${uuid} port=${tmp_port}"
+    cmd="nohup ispn-perf-test.sh -cfg ${props} -name ${node_name} -uuid ${uuid} -port ${tmp_port} -nohup > /dev/null 2> /dev/null < /dev/null &"
+    command="${command} ${cmd}"
+}
+
+
+${command}
--- start-ispn-perf-tests.sh
+++ start-ispn-perf-tests.sh
@@ -5,23 +5,25 @@
 source config.sh
 
 ## The first instance is started synchronously, so we avoid merging
-#./start-ispn-perf-test.sh box1
 
 ## Start the first instance manually !
 
-if [ $# -ne 2 ]; 
-    then echo "start-ispn-perf-tests <from> <to>"
+if [ $# -ne 4 ];
+    then echo "start-ispn-perf-tests <from> <to> <start-port> <number of nodes per instance>"
     exit -1
 fi
 
 from=$1
 to=$2
+bind_port=$3
+num_nodes=$4
 
-echo "Starting IspnPerfTests $from - $to"
+echo "Starting IspnPerfTest $from - $to"
+
 
 for (( i=$from; i<=$to; i++ )) ; {
     echo "starting IspnPerfTest on ${prefix}${i}"
-    ./start-ispn-perf-test.sh ${prefix}${i} ${i} &
+    ./start-ispn-perf-test.sh ${i} ${bind_port} ${num_nodes} &
 
     if [ $((i % 100)) == 0 ]
         then
