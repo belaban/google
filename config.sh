3c1b468ad93973d2b29d3e3fc6efdd553b96515a
Bela Ban <belaban@yahoo.com>
2014-07-24 11:47:11 +0200

    ns

--- config.sh
+++ config.sh
@@ -3,9 +3,9 @@
 # The prefix for all instances, Will be suffixed with an int, e.g. box1, box2 etc
 prefix="m"
 
-#machine_type=n1-highcpu-2
+machine_type=n1-highcpu-4
 #machine_type=n1-highcpu-8
-machine_type=n1-standard-2
+#machine_type=n1-standard-2
 zone=us-central1-b
 #zone=europe-west1-b
 base_port=7800
