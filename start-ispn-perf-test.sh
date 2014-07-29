5e31ddce28257634eb1cd2649c47e52eb1de636b
Bela Ban <belaban@yahoo.com>
2014-07-29 10:57:16 +0200

    Added debug option to start-ispn-xxx scripts

--- start-ispn-perf-test.sh
+++ start-ispn-perf-test.sh
@@ -3,8 +3,8 @@
 
 source config.sh
 
-if [ $# -ne 3 ];
-    then echo "start-ispn-perf-test.sh <instance ID> <bind-port> <number of nodes>"
+if [ $# -ne 4 ];
+    then echo "start-ispn-perf-test.sh <instance ID> <bind-port> <number of nodes> <debug>"
     exit -1
 fi
 
@@ -12,9 +12,19 @@ instance_id=$1
 machine=${prefix}${instance_id}
 bind_port=$2
 num_nodes=$3
+debug=$4
 props=infinispan-google.xml
 ssh_args="--log_level=WARNING --ssh_arg=-x --ssh_arg=-oLogLevel=quiet"
 command="gcutil ssh ${ssh_args} ${machine} "
+executable="ispn-perf-test.sh"
+
+if ${debug};
+  then
+     executable="ispn-perf-test-debug.sh"
+  else
+     executable="ispn-perf-test.sh"
+fi
+
 
 for (( i=1; i<= ${num_nodes}; i++ )) ; {
     node_name=${machine}.${i}
@@ -22,7 +32,7 @@ for (( i=1; i<= ${num_nodes}; i++ )) ; {
     tmp_port=$((${bind_port} + ${i} -1))
     # echo "machine=${machine}: node: ${node_name} uuid: ${uuid} port=${tmp_port}"
     out=/tmp/${node_name}.log
-    cmd="nohup ispn-perf-test.sh -cfg ${props} -name ${node_name} -uuid ${uuid} -port ${tmp_port} -nohup > ${out} 2> ${out} < /dev/null &"
+    cmd="nohup ${executable} -cfg ${props} -name ${node_name} -uuid ${uuid} -port ${tmp_port} -nohup > ${out} 2> ${out} < /dev/null &"
     command="${command} ${cmd}"
 }
 
--- start-ispn-perf-tests.sh
+++ start-ispn-perf-tests.sh
@@ -8,8 +8,8 @@ source config.sh
 
 ## Start the first instance manually !
 
-if [ $# -ne 4 ];
-    then echo "start-ispn-perf-tests <from> <to> <start-port> <number of nodes per instance>"
+if [ $# -ne 5 ];
+    then echo "start-ispn-perf-tests <from> <to> <start-port> <number of nodes per instance> <debug>"
     exit -1
 fi
 
@@ -17,13 +17,14 @@ from=$1
 to=$2
 bind_port=$3
 num_nodes=$4
+debug=$5
 
 echo "Starting IspnPerfTest $from - $to"
 
 
 for (( i=$from; i<=$to; i++ )) ; {
     echo "starting IspnPerfTest on ${prefix}${i}"
-    ./start-ispn-perf-test.sh ${i} ${bind_port} ${num_nodes} &
+    ./start-ispn-perf-test.sh ${i} ${bind_port} ${num_nodes} ${debug} &
 
     if [ $((i % 100)) == 0 ]
         then
